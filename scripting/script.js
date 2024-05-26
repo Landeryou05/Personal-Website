dbbbf616f584dc0c02b9e048ae7bd4a2fd4650be
Landeryou05 <landeryou05@gmail.com>
2024-05-27 00:31:44 +0100

    Major Changes 6

---
+++ scripting/script.js
@@ -0,0 +1,8 @@
+var socialImage = document.getElementById("Platform-Images-ID");
+
+document.getElementById("Platforms-Images-ID").onmouseover = function(){mouseOver()}
+document.getElementById("Platforms-Images-ID").onmouseout = function(){mouseOut()}
+
+function mouseOver(){
+    document.getElementById("Platforms-Images-ID").src = "C:/Users/lande/Documents/PersonalWebsite/ASSETS/SOCIAL/GITHUB WHITE.png"
+}
